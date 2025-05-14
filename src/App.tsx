fe41dafd2669f1fdab44cb95a1ca805c75065e84
prabhasetael <elangsatya08@gmail.com>
2025-05-14 23:46:46 +0700

    basic frontend done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import GroupListPage from './pages/GroupList';
+import MainPage from './pages/MainPage';
+import PostDetailPage from './pages/PostDetail';
+import './App.css';
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/groups" element={<GroupListPage />} />
+        <Route path="/groups/:groupId" element={<MainPage />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/post/:postId" element={<div>Post Detail</div>} />
+        <Route path="/post/:postId/comments" element={<div>Post Comments</div>} />
       </Routes>
     </Router>
   );
